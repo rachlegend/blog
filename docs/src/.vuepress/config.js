bae22f8c95330a6a2abed53dc7f53df3c14d5784
Rachel <ylianggeom@gmail.com>
2022-11-20 13:47:05 +0800

    ???

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,21 +1,16 @@
-const { autoResizerProps } = require('element-plus')
 const { description } = require('../../package')
 
 module.exports = {
   title: 'Salutonly\'s Digital Garden',
-  description: 'salutonly的个人博客',
+  description: description,
   base: '/blog/',
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-  markdown: {
-    lineNumbers: false
-  },
   theme: 'reco',
   themeConfig: {
-    sidebar: 'auto',
     repo: 'https://github.com/salutonly/blog',
     repoLabel: 'Github',
     editLinks: false,
@@ -61,10 +56,6 @@ module.exports = {
       ],
     }
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
